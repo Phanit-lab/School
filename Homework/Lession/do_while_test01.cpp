b1e79c47e8946c932ae49c97d61b3da9b3156de1
Phanit-lab <keosophanit@outlook.com>
2024-06-29 16:49:50 +0700

    calculator test

--- Homework/Lession/do_while_test01.cpp
+++ Homework/Lession/do_while_test01.cpp
@@ -5,7 +5,7 @@ int main(){
 
     //Enter number1: 10
     //Enter number2: 20
-    //Enter operator(+,-,*,/): +
+    //Enter operator(+,-,*,/): 
     //The result is 30
 
     //1. Store
@@ -23,29 +23,47 @@ int main(){
         cout<<"3. multiplication"<<endl;
         cout<<"4. divition"<<endl;
         cout<<"0. Exit"<<endl;
-        cout<<"Enter number1: ";
-        cin>>number1;
-        cout<<"Enter number2: ";
-        cin>>number2;
-        cout<<"Enter operator(+,-,*,/): ";
+        cout<<"Enter choice (0,1,2,3,4): ";
         cin>>op;
+
+
+        if (op==0){
+            return 1;
+        }
         switch(op){
             case 1:
+            cout<<"Enter number1: ";
+            cin>>number1;
+            cout<<"Enter number2: ";
+            cin>>number2;
                 result = number1 + number2;
                 break;
             case 2:
+            cout<<"Enter number1: ";
+            cin>>number1;
+            cout<<"Enter number2: ";
+            cin>>number2;
                 result = number1 - number2;
                 break;
             case 3:
+            cout<<"Enter number1: ";
+            cin>>number1;
+            cout<<"Enter number2: ";
+            cin>>number2;
                 result = number1 * number2;
                 break;
             case 4:
+            cout<<"Enter number1: ";
+            cin>>number1;
+            cout<<"Enter number2: ";
+            cin>>number2;
                 if(number2==0){
                     flag=1;
                 }else{
                     result = number1 / number2;
                 }
                 break;
+        
             default:
                 flag=2;
                 break;
